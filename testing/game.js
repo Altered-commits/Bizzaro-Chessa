e6da4a1cea06f03ad04bb1d9edc49c19befcacaa
skrrrrrrrr <kovid23arora@gmail.com>
2024-08-02 23:47:14 +0530

    letters and numbers added but every second letter is darker in colour for some reason

--- testing/game.js
+++ testing/game.js
@@ -20,23 +20,40 @@ function pieceCaptureOrMove(square, piece) {
 function startGame(){
     const height=8;
     const width=8;
-    document.getElementById("border").innerHTML +=`<div class="ChessBoard" id="board"> </div>`;
-    for ( i =0 ;i<height;i++){
-        document.getElementById("board").innerHTML +=`<div class="ChessRow" id="${i}">`;
-        for ( j =0 ;j<width;j++){
-            if ((i+j)%2==0){ 
-                document.getElementById(`${i}`).innerHTML +=`<div class="ChessSquare White" id=${(i*height+j)}"> </div>`;
-                console.log(`${(i*height+j)}`)
-            }
-            if ((i+j)%2!=0)  { document.getElementById(`${i}`).innerHTML +=`<div class="ChessSquare Black" id=${(i*height+j)}"> </div>`;}
+    document.getElementById("border").innerHTML +=`<div class="ChessBoard" id="board"> </div>`;//adding board so it looks clean when start is not pressed
 
+    for ( i =0 ;i<height;i++){
+        document.getElementById("board").innerHTML +=`<div class="ChessRow" id="row${i}"></div>`;//adding chessrow one by one
+        for ( j =0 ;j<width;j++){
+
+            if ((i+j)%2==0){ 
+                document.getElementById(`row${i}`).innerHTML +=`<div class="ChessSquare White" id="square${i*height+j}"> </div>`;
+                console.log(`${(i*height+j)}`)//white square added
+
+
+                if ((i*height+j)%8==0){
+                    document.getElementById(`square${(i*height+j)}`).innerHTML +=`<div class="ChessSquareNumber BlackNumber">${i+1}</div>`;
+                }//number added
+            }
+            else  { 
+                document.getElementById(`row${i}`).innerHTML +=`<div class="ChessSquare Black" id="square${i*height+j}"> </div>`;
+                console.log(`${(i*height+j)}`)//black square added
+
+                if ((i*height+j)%8==0){
+                    
+                    document.getElementById(`square${(i*height+j)}`).innerHTML +=`<div class="ChessSquareNumber WhiteNumber">${i+1}</div>`;
+                }//number added
         }
-        document.getElementById("board").innerHTML +='</div>';
+        if (i*height+j>=56){
+            if ((i*height+j)%2==0)
+            document.getElementById(`square${(i*height+j)}`).innerHTML +=`<div class="ChessSquareLetter WhiteNumber">${String.fromCharCode(i*height+j+41)}</div>`;
+             //adding ascii value for letters in last row elemebts 
+            else 
+            document.getElementById(`square${(i*height+j)}`).innerHTML +=`<div class="ChessSquareLetter BlackkNumber">${String.fromCharCode(i*height+j+41)}</div>`;
+            }//letter added
+        }
     }
-    document.getElementById("buttons").innerHTML  +=`<button id ="restartButton" onclick="restartGame()">RESTART</button>`;
-    document.getElementById("buttons").innerHTML  +=`<button id ="undoButton" onclick="undoMove()">UNDO</button>`;
-    document.getElementById("buttons").innerHTML  +=`<button id ="quitButton" onclick="quitGame()">QUIT</button>`;
-    document.getElementById("startButton").outerHTML  =``;
+    document.getElementById("buttons").innerHTML  +=`<button id ="restartButton" onclick="restartGame()">RESTART</button><button id ="undoButton" onclick="undoMove()">UNDO</button><button id ="quitButton" onclick="quitGame()">QUIT</button>`;
 }
 //Add event listeners for drag-and-drop
 function setup() {
