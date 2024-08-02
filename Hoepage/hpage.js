e88f6e9dbb290baa6a112b60c7df555d66673378
skrrrrrrrr <kovid23arora@gmail.com>
2024-08-02 11:02:05 +0530

    homepage update

---
+++ Hoepage/hpage.js
@@ -0,0 +1,12 @@
+function startGame(){
+    window.open("../Game/game.html");
+}
+
+function aboutUs(){
+    window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs";
+}
+
+function quitGame(){
+    if(confirm("do you wanna quit fr ong?"))
+    window.close();
+}
