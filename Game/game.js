fea8dd77f493b90fe7a04cff1578eeab602b4f08
Altered-commits <lalith.pecheti@gmail.com>
2024-08-04 19:43:34 +0530

    Reworked chess board...Moved logic from HTML to JS

--- Game/game.js
+++ Game/game.js
@@ -1,11 +1,89 @@
-//Get all chess piece elements
-const chessPieces = document.querySelectorAll('.ChessPiece');
-const chessSquares = document.querySelectorAll('.ChessSquare');
+//All of 'query selectors' or 'get elements'
+const chessBoard = document.querySelector('.ChessBoard')
+
+//Chess constants
+const CHESS_WIDTH = 8;
+const CHESS_PIECE_ARRANGEMENT = [
+    ["BR1", "BN1", "BB1", "BQ1", "BK1", "BB2", "BN2", "BR2"],
+    ["BP1", "BP2", "BP3", "BP4", "BP5", "BP6", "BP7", "BP8"],
+    [],
+    [],
+    [],
+    [],
+    ["WP1", "WP2", "WP3", "WP4", "WP5", "WP6", "WP7", "WP8"],
+    ["WR1", "WN1", "WB1", "WQ1", "WK1", "WB2", "WN2", "WR2"]
+];
+
+//----------Functions----------
+function setupChessBoard()
+{
+    let invertColor = 1;
+
+    for (let row = 0; row < CHESS_WIDTH; row++) {
+        for (let col = 0; col < CHESS_WIDTH; col++) {
+            //Create a square
+            const square = document.createElement("div");
+            square.classList.add("ChessSquare");
+            
+            //Each square gets it's own unique id
+            const nid = ((row * CHESS_WIDTH) + col);
+            square.id = nid;
+            
+            //Square colors decided here
+            if ((row + col) % 2 === 0) {
+                square.classList.add("White");
+            } else {
+                square.classList.add("Black");
+            }
+            
+            //Adding ranks
+            if(nid % 8 === 0) {
+                const rank = document.createElement("div");
+
+                rank.classList.add("ChessSquareNumber");
+                rank.classList.add(invertColor === 1 ? "BlackNumber" : "WhiteNumber");
+                rank.innerHTML = row;
+                
+                square.append(rank);
+                
+                //Invert color
+                invertColor = invertColor ^ 1;
+            }
+
+            //Adding files
+            if(nid > 55) {
+                const file = document.createElement("div");
+
+                file.classList.add("ChessSquareLetter");
+                file.classList.add(invertColor === 0 ? "BlackNumber" : "WhiteNumber");
+                file.innerHTML = String.fromCharCode(nid + 41); //ascii of 'a' starts from 97 hence the offset (+ 41)
+
+                square.append(file);
+                
+                //Invert color
+                invertColor = invertColor ^ 1;
+            }
+            
+            //Add pieces to square if they exist
+            const piece = CHESS_PIECE_ARRANGEMENT[row][col]
+
+            if(piece) {
+                const pieceDiv = document.createElement("div");
+
+                pieceDiv.classList.add("ChessPiece");
+                pieceDiv.id        = piece;
+                pieceDiv.innerHTML = `<img src='ChessPiecesPng/${piece.slice(0, -1)}.png'>`;
+
+                square.appendChild(pieceDiv);
+            }
+
+            chessBoard.append(square);
+        }
+    }
+}
 
 function pieceCaptureOrMove(square, piece) {
     const existingPiece = square.querySelector(".ChessPiece");
-
-    console.log(existingPiece);
 
     if(existingPiece) {
         if (existingPiece.id[0] !== piece.id[0]) {
@@ -17,8 +95,11 @@ function pieceCaptureOrMove(square, piece) {
     }
 }
 
-//Add event listeners for drag-and-drop
-function setup() {
+// Add event listeners for drag-and-drop
+function setupDragDropEvents() {
+    const chessPieces  = document.querySelectorAll('.ChessPiece');
+    const chessSquares = document.querySelectorAll('.ChessSquare');
+
     chessPieces.forEach((piece) => {
         piece.setAttribute('draggable', true);
         
@@ -44,22 +125,24 @@ function setup() {
     });
 }
 
-window.addEventListener('DOMContentLoaded', (e) => {
-    setup();
-});
-
-function restartGame(){
+function restartGame() {
     location.reload();
 }
 
-function quitGame(){
+function quitGame() {
     if(confirm("do you wanna quit fr ong?"))
-    window.close();
+        window.close();
 }
+
+//----------EVENT LISTENERS FOR DOCUMENTS AND WINDOWS----------
+window.addEventListener('DOMContentLoaded', (e) => {
+    setupChessBoard();
+    setupDragDropEvents();
+});
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'r' || event.key === 'R') {
-        event.preventDefault(); // Optional: prevent the default action if needed
-        location.reload(); // Reload the page
+        event.preventDefault();
+        location.reload();
     }
 });
--- Homepage/hpage.js
+++ Homepage/hpage.js
@@ -1,12 +1,12 @@
-function startGame(){
+function startGame() {
     window.open("../Game/game.html");
 }
 
-function aboutUs(){
+function aboutUs() {
     window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2xs";
 }
 
-function quitGame(){
+function quitGame() {
     if(confirm("do you wanna quit fr ong?"))
     window.close();
 }
